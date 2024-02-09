d292d4503498a7b556fdd41106b53e52dd130169
cynical-cheddar <nadroj.taylor99@gmail.com>
2024-02-09 14:39:22 +0000

    Update nuke.sh

--- nuke.sh
+++ nuke.sh
@@ -2,7 +2,9 @@
 #----------------------------------------------------------
 # test bash if
 
-echo This tool is DESTRUCTIVE. Ensure that you are using this on the correct account! Press enter to continue
+echo This tool is DESTRUCTIVE. Ensure that you are using this on the correct account! 
+echo Cross-reference the account-name (located in the top-right) with the account-name in the central spreadsheet.
+echo Press enter to continue (or exit with ctrl + c)
 
 read x
 
