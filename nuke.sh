716483723e7ef9c9abd52619fbb8a06f2cef17f2
cynical-cheddar <nadroj.taylor99@gmail.com>
2024-02-09 14:25:49 +0000

    Update nuke.sh

--- nuke.sh
+++ nuke.sh
@@ -2,6 +2,10 @@
 #----------------------------------------------------------
 # test bash if
 
+echo This tool is DESTRUCTIVE. Ensure that you are using this on the correct account! Press enter to continue
+
+read x
+
 sudo wget https://github.com/gruntwork-io/cloud-nuke/releases/download/v0.1.24/cloud-nuke_linux_amd64
 sudo mv cloud-nuke_linux_amd64 /usr/local/bin/cloud-nuke
 cd /usr/local/bin
